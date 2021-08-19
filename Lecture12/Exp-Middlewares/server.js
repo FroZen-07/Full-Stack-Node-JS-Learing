1339842d4acba93819b641b553ac4f17e6efd097
Bitan Atarthy <bitan.atarthy07@gmail.com>
2021-08-19 14:44:10 +0530

    middle wares

---
+++ Lecture12/Exp-Middlewares/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+
+// Middle Wares Stack costs memory
+function m1 (req, res, next) {
+    console.log('m1 pre-next')
+    next()
+    console.log('m1 post-next')
+}
+function m2 (req, res, next) {
+    console.log('m2 pre-next')
+    next()
+    console.log('m2 post-next')
+}
+function m3 (req, res, next) {
+    console.log('m3 pre-next')
+    next()
+    console.log('m3 post-next')
+}
+
+
+app.get('/hello', m1, m2, m3, (req, res) => {
+    console.log('pre-send')
+    res.send('Hello World')
+    console.log('post-send')
+})
+
+
+
+app.listen(3000, () => {
+    console.log('server started on http://localhost:3000')
+})
